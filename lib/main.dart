11ada39dc6d0a8b4bbd6f52d4f6a7d12c58cdef2
naphat <natwolfshadow@gmail.com>
2026-08-08 11:09:04 +0700

    Step 3: Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,21 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Hello World'));
+    return Center(
+      child: Container(
+        color: Colors.amber,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(50),
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
 
